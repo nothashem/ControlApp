fc64d03e0b565c9eb70cbe4818c82850eaa4a463
Yazeed AlKhalaf <yazeedfady@gmail.com>
2024-04-28 15:38:07 +0300

    make the cards scrollabe

--- Control/Control/ContentView.swift
+++ Control/Control/ContentView.swift
@@ -7,17 +7,58 @@
 
 import SwiftUI
 
+struct Card: Identifiable {
+    let id = UUID()
+    var balance: Int64
+    var currency: String
+    var last4Digits: String
+}
+
 struct ContentView: View {
+    let cards: [Card] = [
+        Card(
+            balance: Int64(3140),
+            currency: "SAR",
+            last4Digits: "4039"
+        ),
+        Card(
+            balance: Int64(1853),
+            currency: "SAR",
+            last4Digits: "0945"
+        ),
+        Card(
+            balance: Int64(144),
+            currency: "SAR",
+            last4Digits: "1264"
+        ),
+        Card(
+            balance: Int64(146),
+            currency: "SAR",
+            last4Digits: "2362"
+        ),
+        Card(
+            balance: Int64(953),
+            currency: "SAR",
+            last4Digits: "1536"
+        )
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            CardComponent(balance: Int64(1560), currency: "SAR", last4Digits: "3429")
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(cards) { card in
+                        CardComponent(
+                            balance: card.balance,
+                            currency: card.currency,
+                            last4Digits: card.last4Digits
+                        )
+                        .padding()
+                    }
+                }
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
